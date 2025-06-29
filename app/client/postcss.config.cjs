5123c36b6e2ba3f43f1ab843226a33f17ec6fec8
wyatts97 <wyattlonerboy97@gmail.com>
2025-06-29 01:54:17 -0500

    Update

--- app/client/postcss.config.cjs
+++ app/client/postcss.config.cjs
@@ -1,9 +1,9 @@
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
+const tailwindcss = require('@tailwindcss/postcss');
+const autoprefixer = require('autoprefixer');
 
-export default {
+module.exports = {
   plugins: [
     tailwindcss(),
     autoprefixer(),
   ],
-}
+};
